4f1464b1c2210637203c787734b7f5e4af19717b
Stef Lewandowski <stef@stef.io>
2011-12-10 12:02:39 +0000

    Be more specific for Ruby 1.8.7

--- lib/compass_twitter_bootstrap.rb
+++ lib/compass_twitter_bootstrap.rb
@@ -8,5 +8,5 @@ require 'compass'
 Compass::Frameworks.register("twitter_bootstrap", :path => "#{File.dirname(__FILE__)}/..")
 
 module CompassTwitterBootstrap
-  STYLESHEETS = File.expand_path("../stylesheets", __FILE__)
+  CompassTwitterBootstrap::STYLESHEETS = File.expand_path("../stylesheets", __FILE__)
 end
